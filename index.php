67b39bd95f42760ba533b82ae267ac4ed747b6f5
DiaMia8 <dianaszanto@gmail.com>
2019-12-12 19:36:38 +0100

    classok és első lekérdezés majd oldal hozzáadása

--- index.php
+++ index.php
@@ -4,10 +4,14 @@ spl_autoload_register(function ($class) {
     include('src/' . $class . ".php");
 });
 
+use modell\Konyv;
+
 $conf=include("config.php");
 
-$pdo = new \PDO($conf["dsn"],$conf['user'],$conf['password']);
-
+$pdo = new \PDO($conf['db']['dsn'],$conf['db']['user'],$conf['db']['password']);
+$sql = "SELECT * FROM konyv";
+$statement = $pdo->prepare($sql);  // https://www.php.net/manual/en/class.pdostatement.php
+$statement->execute();
 
 
 ?>
@@ -22,6 +26,46 @@ $pdo = new \PDO($conf["dsn"],$conf['user'],$conf['password']);
 </head>
 <body>
 <h1>Könyveim</h1>
+<table id="nybooks">
+    <tr>
+        <th>
+            Cim
+        </th>
+        <th>
+            Szerző
+        </th>
+        <th>
+            Kategória
+        </th>
+        <th>
+            Kiadó
+        </th>
+        <th>
+            Oldalainak száma
+        </th>
 
+    </tr>
+    <?php /** @var $konyv Konyv */?>
+    <?php while($konyv = $statement->fetchObject(Konyv::class)) : ?>
+        <tr>
+            <td>
+                <?= $konyv->getCim(); ?>
+            </td>
+            <td>
+                <?= $konyv->getSzerzo(); ?>
+            </td>
+            <td>
+                <?= $konyv->getKategoria(); ?>
+            </td>
+            <td>
+                <?= $konyv->getKiado(); ?>
+            </td>
+            <td>
+                <?= $konyv->getOldalakSzama(); ?> oldal
+            </td>
+
+        </tr>
+    <?php endwhile; ?>
+</table>
 </body>
 </html>
