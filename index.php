24c984b1b4c7980b67e6bb4d5c0a26c3ac23a7a5
DiaMia8 <dianaszanto@gmail.com>
2019-12-12 18:54:00 +0100

    index.php pdo változó létrehozva

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
 <?php
+
+spl_autoload_register(function ($class) {
+    include('src/' . $class . ".php");
+});
+
+$conf=include("config.php");
+
+$pdo = new \PDO($conf["dsn"],$conf['user'],$conf['password']);
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Könyveim</title>
+</head>
+<body>
+<h1>Könyveim</h1>
+
+</body>
+</html>
